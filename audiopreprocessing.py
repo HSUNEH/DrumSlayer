e1c1707283377d03b526ced7f511f093b8815827
HSUNEH <iamsuntae@gmail.com>
2023-08-01 13:08:13 +0900

    encoderdecoder

--- audiopreprocessing.py
+++ audiopreprocessing.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def audio_to_log_mel_spectrogram(audio_path, n_mels=128, hop_length=512, n_fft=2048, sr = 48000):
     # Load audio file
-    y, sr = librosa.load(audio_path, sr)
+    y, sr = librosa.load(audio_path, sr = sr)
 
     # Compute mel spectrogram
     mel_spectrogram = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=n_mels, hop_length=hop_length, n_fft=n_fft)
@@ -19,12 +19,12 @@ def audio_to_log_mel_spectrogram(audio_path, n_mels=128, hop_length=512, n_fft=2
 #     audio_path = "path/to/your/audio_file.wav"  # Replace with the path to your audio file
 #     log_mel_spec = audio_to_log_mel_spectrogram(audio_path)
 
-    # # Display the log mel spectrogram
-    # import librosa.display
-    # import matplotlib.pyplot as plt
-    # plt.figure(figsize=(10, 4))
-    # librosa.display.specshow(log_mel_spec, x_axis='time', y_axis='mel', sr=sr, hop_length=hop_length)
-    # plt.colorbar(format='%+2.0f dB')
-    # plt.title('Log Mel Spectrogram')
-    # plt.tight_layout()
-    # plt.show()
+#     # Display the log mel spectrogram
+#     import librosa.display
+#     import matplotlib.pyplot as plt
+#     plt.figure(figsize=(10, 4))
+#     librosa.display.specshow(log_mel_spec, x_axis='time', y_axis='mel', sr=sr, hop_length=hop_length)
+#     plt.colorbar(format='%+2.0f dB')
+#     plt.title('Log Mel Spectrogram')
+#     plt.tight_layout()
+#     plt.show()
--- main.py
+++ main.py
@@ -1,24 +1,36 @@
-# TODO : audio -> Log mel spectrogram -> encoding -> decoding -> MIDI file
-# Baseline: Onset and frames 
+# TODO 1) audio -> Log mel spectrogram -> encoding -> decoding -> MIDI file
+# Baseline 1) : Encoder Decoder (Onset + DrummerNet)
+
+# TODO 2): MIDI file  <-> midi token 
+# Baseline 2) : MT3 - T5 transformer 
+
 
 # audiopreprocessing :  audio -> log mel
 # model :               encoding & decoding
 
 
-# TODO : pytorch lightning
+## pytorch lightning 
+# essential 
+# 1) Model (with train_step, configure_optimizers)
+# 2) dataset
+# 3) trainer = L.Trainer()
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader
+from torch.utils.data import Dataset, DataLoader
 import lightning as L
 from audiopreprocessing import audio_to_log_mel_spectrogram
+import librosa
+import numpy
+import os
+
 
 class Autoencoder(L.LightningModule):
     def __init__(self):
         super(Autoencoder, self).__init__()
         self.encoder = nn.Sequential(
-            nn.Linear(28 * 28, 128),
+            nn.Linear(128*188, 128),
             nn.ReLU(),
             nn.Linear(128, 64),
             nn.ReLU(),
@@ -29,7 +41,7 @@ class Autoencoder(L.LightningModule):
             nn.ReLU(),
             nn.Linear(64, 128),
             nn.ReLU(),
-            nn.Linear(128, 28 * 28),
+            nn.Linear(128, 128*188),
             nn.Sigmoid()  # Output should be between 0 and 1 (image pixel values)
         )
 
@@ -43,32 +55,48 @@ class Autoencoder(L.LightningModule):
         x = x.view(x.size(0), -1)
         y_pred = self(x)
         loss = nn.MSELoss()(y_pred, x)
-        self.log('train_loss', loss)
+        self.log('train_loss', loss, prog_bar=True, on_step=False, on_epoch=True)
         return loss
 
     def configure_optimizers(self):
         return optim.Adam(self.parameters(), lr=1e-3)
 
-audio_path = "path/to/your/audio_file.wav"  # Replace with the path to your audio file
-log_mel_spec = audio_to_log_mel_spectrogram(audio_path)
 
 
-# Parameters
-n_mels=128
-hop_length=512 
-n_fft=2048
-sr = 48000
+class SpectrogramDataset(Dataset):
+    def __init__(self, data_folder, sr=48000, n_mels=128):
+        self.data_folder = data_folder
+        self.sr = sr
+        self.n_mels = n_mels
+        self.wav_files = [file for file in os.listdir(data_folder) if file.endswith('.wav')]
 
-# if __name__ == "__main__":
-#     # 데이터 전처리
-#     transform = transforms.Compose([transforms.ToTensor()])
-#     mnist_train = MNIST('data', train=True, download=True, transform=transform)
-#     mnist_train_loader = DataLoader(mnist_train, batch_size=64, shuffle=True)
+    def __len__(self):
+        return len(self.wav_files)
 
-#     # 모델 및 Trainer 생성
-#     model = Autoencoder()
-#     trainer = L.Trainer(max_epochs=10, gpus=1)
+    def __getitem__(self, idx):
+        wav_file = os.path.join(self.data_folder, self.wav_files[idx])
+        spectrogram = audio_to_log_mel_spectrogram(wav_file, sr=self.sr, n_mels=self.n_mels)
+        return torch.tensor(spectrogram, dtype=torch.float32).unsqueeze(0)
 
-#     # 학습 시작
-#     trainer.fit(model, mnist_train_loader)
+
+
+if __name__ == "__main__":
+    # Parameters
+    n_mels=128
+    hop_length=512 
+    n_fft=2048
+    sr = 48000
+
+    # 데이터 전처리 
+    data_folder = 'midi_2_wav/samples'
+    batch_size = 2
+    dataset = SpectrogramDataset(data_folder)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+    # 모델 및 Trainer 생성
+    model = Autoencoder()
+    trainer = L.Trainer(max_epochs=100)
+
+    # 학습 시작
+    trainer.fit(model, dataloader)
  
