32329f408cded9f047da29b8897104496cc9d561
HSUNEH <iamsuntae@gmail.com>
2023-12-14 18:19:41 +0900

    arg default change

--- data_generate/data_generate.py
+++ data_generate/data_generate.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     parser.add_argument('--midi_number', type=int, default=10, help='midi number')
     # parser.add_argument('--beat', type=int, default=1, help='beat')
     parser.add_argument('--loop_seconds', type=int, default=5, help='loop_seconds')
-    parser.add_argument('--sample_rate', type=int, default=48000, help='sample_rate')
+    parser.add_argument('--sample_rate', type=int, default=44100, help='sample_rate')
     parser.add_argument('--grid_random', type=str, default='RG', help='R for random, G for grid, RG for random in grid, GG for gaussian in grid')
     parser.add_argument('--mono', type=bool, default=False, help='mono or stereo')
     parser.add_argument('--oneshot_dir', type=str, default='./midi_2_wav/one_shots/', help='input data directory')
